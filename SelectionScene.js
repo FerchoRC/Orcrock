3ef06c3075bba982f5f52e1cc691579876740186
Ruiz <samurestudio@gmail.com>
2024-11-11 11:37:44 -0500

    Inventario
    Inventario funcional y layout del nivel 1

--- SelectionScene.js
+++ SelectionScene.js
@@ -4,32 +4,14 @@ export class SelectionScene extends Phaser.Scene {
     }
 
     preload() {
-
         console.log("Cargando recursos...");
 
         // Cargar la imagen de fondo
-        this.load.image(
-            'background',
-            'assets/bricks.png'
-        )
+        this.load.image('background', 'assets/bricks.png');
 
-        this.load.spritesheet(
-            'tileseat',
-            'assets/demo.png',
-            {frameWidth: 16, frameHeight: 16}
-        )
-
-        this.load.spritesheet(
-            'frameTable',
-            'assets/Marco02.png',
-            {frameWidth: 36, frameHeight: 27}
-        )
-
-        this.load.spritesheet(
-            'table',
-            'assets/tableBack02.png',
-            {frameWidth: 56, frameHeight:56}
-        )
+        this.load.spritesheet('tileseat', 'assets/demo.png', { frameWidth: 16, frameHeight: 16 });
+        this.load.spritesheet('frameTable', 'assets/Marco02.png', { frameWidth: 36, frameHeight: 27 });
+        this.load.spritesheet('table', 'assets/tableBack02.png', { frameWidth: 56, frameHeight: 56 });
 
         // Cargar las imágenes de los objetos
         this.load.image('daga', 'assets/daga.png');
@@ -38,14 +20,10 @@ export class SelectionScene extends Phaser.Scene {
     }
 
     create() {
-
-         // Iniciar la transición con un fade-in
-       this.cameras.main.fadeIn(1000, 0, 0, 0);
-
+        this.cameras.main.fadeIn(1000, 0, 0, 0);
         console.log("Se está creando la escena");
-        this.add.image(400, 900, 'background'); // Añadir el fondo
 
-        // Escalar el fondo
+        // Fondo escalado
         let background = this.add.image(this.sys.game.config.width / 2, this.sys.game.config.height / 2, 'background');
         background.setDisplaySize(this.sys.game.config.width, this.sys.game.config.height);
 
@@ -58,56 +36,22 @@ export class SelectionScene extends Phaser.Scene {
         const object3 = this.add.image(centerX + 400, centerY, 'espada').setOrigin(0.5).setScale(1.5).setInteractive();
 
         // Añadir texto debajo de cada objeto
-        const dagaText = this.add.text(centerX - 400, centerY + 100, 'Daga', {
-        fontFamily: 'MedievalSharp',
-        fontSize: '30px',
-        color: '#ffffff',
-        align: 'center'
-        }).setOrigin(0.5);
-
-        const mazaText = this.add.text(centerX, centerY + 100, 'Maza', {
-        fontFamily: 'MedievalSharp',
-        fontSize: '30px',
-        color: '#ffffff',
-        align: 'center'
-        }).setOrigin(0.5);
-
-        const espadaText = this.add.text(centerX + 400, centerY + 100, 'Espada', {
-        fontFamily: 'MedievalSharp',
-        fontSize: '30px',
-        color: '#ffffff',
-        align: 'center'
-        }).setOrigin(0.5);
+        this.add.text(centerX - 400, centerY + 100, 'Daga', { fontFamily: 'MedievalSharp', fontSize: '30px', color: '#ffffff', align: 'center' }).setOrigin(0.5);
+        this.add.text(centerX, centerY + 100, 'Maza', { fontFamily: 'MedievalSharp', fontSize: '30px', color: '#ffffff', align: 'center' }).setOrigin(0.5);
+        this.add.text(centerX + 400, centerY + 100, 'Espada', { fontFamily: 'MedievalSharp', fontSize: '30px', color: '#ffffff', align: 'center' }).setOrigin(0.5);
 
         // Detectar clics en los objetos y hacer fade antes de cambiar de escena
-        object1.on('pointerdown', () => {
-        this.cameras.main.fadeOut(1000, 0, 0, 0); // Iniciar fade
-        this.cameras.main.once('camerafadeoutcomplete', () => {
-        this.selectObject('daga');
-        this.scene.start('game');
-        });
-        });
-
-        object2.on('pointerdown', () => {
-        this.cameras.main.fadeOut(1000, 0, 0, 0); // Iniciar fade
-        this.cameras.main.once('camerafadeoutcomplete', () => {
-        this.selectObject('maza');
-        this.scene.start('game');
-        });
-        });
-
-        object3.on('pointerdown', () => {
-        this.cameras.main.fadeOut(1000, 0, 0, 0); // Iniciar fade
-        this.cameras.main.once('camerafadeoutcomplete', () => {
-        this.selectObject('espada');
-        this.scene.start('game');
-        });
-        });
+        object1.on('pointerdown', () => this.selectObject('daga'));
+        object2.on('pointerdown', () => this.selectObject('maza'));
+        object3.on('pointerdown', () => this.selectObject('espada'));
     }
 
     selectObject(object) {
         console.log('Objeto seleccionado:', object);
-        // Aquí puedes cambiar a la escena principal y guardar el objeto seleccionado
-        this.scene.start('Game', { selectedObject: object });
+        // Iniciar la transición con fade-out y pasar a la escena Game con el objeto seleccionado
+        this.cameras.main.fadeOut(1000, 0, 0, 0);
+        this.cameras.main.once('camerafadeoutcomplete', () => {
+            this.scene.start('game', { selectedObject: object });
+        });
     }
-}    
+}
